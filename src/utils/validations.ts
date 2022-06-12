0f90abcce3969d3f75f14d01f86889387838ed0b
Sabir Hassan <sabirbk06@gmail.com>
2022-06-13 02:56:49 +0500

    add validation for update user action

---
+++ src/utils/validations.ts
@@ -0,0 +1,20 @@
+import Joi from 'joi'
+
+const usernameSchema = Joi.string().alphanum().min(3).max(16)
+const passwordSchema = Joi.string().min(6).max(1024)
+const emailSchema = Joi.string().email()
+
+export const updateUserValidation = (
+  data: any,
+  isAdmin: boolean = false
+): Joi.ValidationResult => {
+  const validationChecks: any = {
+    username: usernameSchema,
+    password: passwordSchema,
+    email: emailSchema
+  }
+  if (isAdmin) {
+    validationChecks.isAdmin = Joi.boolean()
+  }
+  return Joi.object(validationChecks).validate(data)
+}

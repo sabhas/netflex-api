c8fabedb8b7e220e71951421af25a72f7f1b9db3
Sabir Hassan <sabirbk06@gmail.com>
2022-06-14 00:19:59 +0500

    implement login api end point

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers'
-import { registerUserValidation } from 'src/utils/validations'
+import { registerUserValidation, loginValidation } from 'src/utils/validations'
 
 const authRouter = Router()
 const controller = new AuthController()
@@ -15,5 +15,17 @@ authRouter.post('/register', async (req, res) => {
     res.status(201).json(user)
   } catch (err) {
     res.status(500).json(err)
+  }
+})
+
+authRouter.post('/login', async (req, res) => {
+  const { error, value: body } = loginValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  try {
+    const response = await controller.login(body)
+    res.send(response)
+  } catch (err: any) {
+    res.status(403).send(err.toString())
   }
 })
